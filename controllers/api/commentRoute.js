57900e350de19df90881f8bd170a46948fed703c
AR31313 <gurlgotgame@gmail.com>
2022-06-11 01:32:35 -0700

    dashboard

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -28,20 +28,34 @@ router.get('/:id', (req, res) => {
 });
 
 //Create a comment
-router.post('/', withAuth, (req, res) => {
-  // check the session
-  if (req.session) {
-    Comment.create({
-      comment_text: 'This is test text.',
-      post_id: req.body.driver_id,
-      // use the id from the session
-      user_id: req.session.user_id,
-    })
-      .then((dbCommentData) => res.json(dbCommentData))
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json(err);
-      });
+// router.post('/', withAuth, (req, res) => {
+//   // check the session
+//   if (req.session) {
+//     Comment.create({
+//       comment_text: 'This is test text.',
+//       post_id: req.body.driver_id,
+//       // use the id from the session
+//       user_id: req.session.user_id,
+//     })
+//       .then((dbCommentData) => res.json(dbCommentData))
+//       .catch((err) => {
+//         console.log(err);
+//         res.status(400).json(err);
+//       });
+//   }
+// });
+
+router.post('/', async (req, res) => {
+  try {
+    req.body.user_id = req.session.user_id;
+    req.body.post_id = Number(req.body.post_id);
+    console.log(req.body);
+
+    const commentData = await Comment.create(req.body);
+    console.log('commentData', commentData);
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
